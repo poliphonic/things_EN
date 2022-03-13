70e6f13e900bbcf7250184c601e35b119b4a908b
poli <poliphonic@gmail.com>
2022-03-13 22:42:46 +0300

    13 Mar 2022

---
+++ quadratic.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def quadratic(a, b, c):
+    """
+    a, b, c: the numbers from a quadratic equation 
+        a * x ** 2 + b * x + c = 0
+    return: one or two values of equation or a message about no roots
+    """
+    d = b ** 2 - 4 * a * c
+    if d > 0:
+        x1, x2 = (-b + d ** .5) / (2 * a), (-b - d ** .5) / (2 * a)
+        return f'x1 = {x1}, x2 = {x2}'
+    if d < 0:
+        return 'the equation has no roots'
+    else:
+        return f'x = {-b / (2 * a)}'
+
+
+print(quadratic(3, 7, -10))
